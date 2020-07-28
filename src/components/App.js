437c8e76cfc60414dcfb89f514946c28ab99e007
David Orson <onlyorson@outlook.com>
2020-07-28 07:49:30 +0100

    Context System

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import UserCreate from "./userCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        <ColorContext.Provider value="green">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
   }
 }
 
